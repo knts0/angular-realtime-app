c1a00e823546afcad5e418c077168994d8d45a37
Kana Otawara <shimmer22.0204@gmail.com>
2021-12-25 16:35:00 +0900

    create amplify/backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "angularrealtimeapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
