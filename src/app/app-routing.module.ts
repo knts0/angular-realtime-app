8a4c9b4c6caa514866fe0b5108080c7dc787c813
Kana Otawara <shimmer22.0204@gmail.com>
2021-12-26 20:19:33 +0900

    add routing of operator side

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CustomerSideComponent } from './customer-side/customer-side.component';
+import { OperatorSideComponent } from './operator-side/operator-side.component';
 
 const routes: Routes = [
-  { path: 'customer-side', component: CustomerSideComponent }
+  { path: 'customer-side', component: CustomerSideComponent },
+  { path: 'operator-side', component: OperatorSideComponent },
 ];
 
 @NgModule({
