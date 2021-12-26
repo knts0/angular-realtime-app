6e68cb3ab8a5b52f8d318de38fa7876ea007a76d
Kana Otawara <shimmer22.0204@gmail.com>
2021-12-26 20:22:53 +0900

    create top component & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CustomerSideComponent } from './customer-side/customer-side.component';
 import { OperatorSideComponent } from './operator-side/operator-side.component';
+import { TopComponent } from './top/top.component';
 
 const routes: Routes = [
   { path: 'customer-side', component: CustomerSideComponent },
   { path: 'operator-side', component: OperatorSideComponent },
+  { path: '', component: TopComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerSideComponent } from './customer-side/customer-side.component';
 import { OperatorSideComponent } from './operator-side/operator-side.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerSideComponent,
-    OperatorSideComponent
+    OperatorSideComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
