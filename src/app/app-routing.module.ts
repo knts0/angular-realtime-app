afc5e8b8b39803deebd044cc302795698e4438ea
Kana Otawara <shimmer22.0204@gmail.com>
2021-12-26 15:15:54 +0900

    add routing of customer-side component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CustomerSideComponent } from './customer-side/customer-side.component';
+
+const routes: Routes = [
+  { path: 'customer-side', component: CustomerSideComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
